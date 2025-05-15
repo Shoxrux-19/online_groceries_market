1e91a77ab3d2cabfe47fb3bcfece888a94632b5b
Shoxrux-19 <muzaffarovshoxrux2005@gmail.com>
2025-05-15 21:24:59 +0500

    Local changes saved before pull

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -14,10 +14,8 @@ class ProfileScreen extends StatelessWidget {
             Row(
               children: [
                 CircleAvatar(
-                  radius: 30,
-                  foregroundImage: NetworkImage(
-                    "https://www.svgrepo.com/show/404545/avatar-man-profile-user-3.svg",
-                  ),
+                  radius: 24,
+                  backgroundImage: AssetImage("assets/images/profile_pic.png"),
                 ),
                 SizedBox(width: 16),
                 Column(
@@ -27,7 +25,7 @@ class ProfileScreen extends StatelessWidget {
                       children: [
                         Text(
                           "Shoxrux",
-                          style: TextStyle(fontSize: 20),
+                          style: TextStyle(fontSize: 20, fontFamily: "Gilroy"),
                         ),
                         SizedBox(width: 8),
                         SvgPicture.asset(
@@ -40,33 +38,70 @@ class ProfileScreen extends StatelessWidget {
                     SizedBox(height: 4),
                     Text(
                       "muzaffarov@gmail.com",
-                      style: TextStyle(fontSize: 16, color: const Color.fromARGB(255, 101, 100, 100)),
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: const Color.fromARGB(255, 101, 100, 100),
+                      ),
                     ),
                   ],
                 ),
               ],
             ),
-            SizedBox(height: 30,),
+            SizedBox(height: 30),
             Divider(),
+            SizedBox(height: 24),
             Row(
-              spacing: 20,
+              children: [
+                SvgPicture.asset("assets/svgs/bag.svg", height: 20, width: 18),
+                SizedBox(width: 20),
+                Text(
+                  "Orders",
+                  style: TextStyle(fontSize: 20, fontFamily: "Gilroy"),
+                ),
+                Spacer(),
+                SvgPicture.asset("assets/svgs/strelka.svg"),
+              ],
+            ),
+            SizedBox(height: 24),
+
+            Divider(),
+            SizedBox(height: 24),
+            Row(
               children: [
                 SvgPicture.asset(
-                  "assets/svgs/bag.svg",
+                  "assets/svgs/id_card.svg",
                   height: 20,
                   width: 18,
                 ),
-                
-                Text("Orders", style: TextStyle(fontSize: 20),),
+                SizedBox(width: 20),
+                Text(
+                  "My Details",
+                  style: TextStyle(fontSize: 20, fontFamily: "Gilroy"),
+                ),
                 Spacer(),
-                SvgPicture.asset("assets/svgs/strelka.svg",
-                )
+                SvgPicture.asset("assets/svgs/strelka.svg"),
               ],
             ),
-            Divider()
+            SizedBox(height: 24),
+            Divider(),
+
+            Row(
+              children: [
+                SvgPicture.asset(
+                  "assets/svgs/location.svg",
+                  height: 20,
+                  width: 18,
+                ),
+                SizedBox(width: 20),
+                Text(
+                  "Delivery Address",
+                  style: TextStyle(fontSize: 20, fontFamily: "Gilroy"),
+                ),
+                Spacer(),
+              ],
+            ),
           ],
         ),
-
       ),
     );
   }
